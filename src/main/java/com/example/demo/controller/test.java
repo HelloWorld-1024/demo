5b38c7924ed71b5e25ad5bd1f0c9964b83a130df
pengwenchao <564845017@qq.com>
2020-12-26 12:10:14 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/controller/test.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: duyubo
+ * @date: 2020年12月26日, 0026 12:03
+ * @description:
+ */
+
+@RestController
+public class test {
+
+    @GetMapping("test")
+    public void test() {
+
+    }
+}
